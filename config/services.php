a8531fca08fa84e13e84c7b621ecbe4996317759
povilas <p.vaitonis@gmail.com>
2017-12-06 20:07:03 +0200

    Added fb service, package config set up.

--- config/services.php
+++ config/services.php
@@ -34,5 +34,10 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
+    'facebook' => [
+        'client_id' => 'xxxxxxx',
+        'client_secret' => 'xxxxxxxx',
+        'redirect' => '',
+    ],
 
 ];
